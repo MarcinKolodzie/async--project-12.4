46983e2ceafaeb3426502ad832c9e3a5beb8451a
Marcin Kołodziej <marcin1.kolodziej@gmail.com>
2022-11-05 14:29:41 +0100

    App is unaware of its container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Textarea from './Textarea'
 
 export class App {
-    constructor(selector) {
-        this.container = document.querySelector(selector)
-
-        this.render()
-    }
+    constructor() {}
 
     render() {
-        this.container.innerHTML = ''
+        const container = document.createElement('div')
 
         const textareaElement1 = new Textarea(
             '{ name: "Marcin" }', 
@@ -22,8 +18,10 @@ export class App {
             )
              
         
-        this.container.appendChild(textareaElement1.render())
-        this.container.appendChild(textareaElement2.render())
+        container.appendChild(textareaElement1.render())
+        container.appendChild(textareaElement2.render())
+
+        return container
     }
 }
 
